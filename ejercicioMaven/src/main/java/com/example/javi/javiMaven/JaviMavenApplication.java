a432671dc40734533678938a3cc6d60954b62547
Javier Alejandro <63244170+Javieralejandro86@users.noreply.github.com>
2023-05-31 10:31:08 +0200

    Add files via upload

---
+++ ejercicioMaven/src/main/java/com/example/javi/javiMaven/JaviMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.javi.javiMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaviMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaviMavenApplication.class, args);
+	}
+
+}
---
+++ ejercicioMaven/src/test/java/com/example/javi/javiMaven/JaviMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javi.javiMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaviMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
