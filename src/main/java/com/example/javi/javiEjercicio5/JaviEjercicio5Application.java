5ab472dfab592c33ec2a7ca88cbbb72ffc41d514
Javier Alejandro <63244170+Javieralejandro86@users.noreply.github.com>
2023-05-31 12:24:20 +0200

    Add files via upload

---
+++ src/main/java/com/example/javi/javiEjercicio5/JaviEjercicio5Application.java
@@ -0,0 +1,13 @@
+package com.example.javi.javiEjercicio5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaviEjercicio5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaviEjercicio5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javi/javiEjercicio5/JaviEjercicio5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javi.javiEjercicio5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaviEjercicio5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
